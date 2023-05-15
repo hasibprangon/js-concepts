b2e15c5aa851a0422a3351a0e3c5f9f342ccaf45
hasibprangon <hasibulhossainp09@gmail.com>
2023-05-15 13:50:24 +0600

    global and functional context

--- concepts.js
+++ concepts.js
@@ -1,1 +1,7 @@
-let a = 5;
+let a = 5;//global context 
+function add(num1, num2){
+    const result = num1 + num2 + a;//function conterxt
+    return result;//function conterxt
+};
+const sum = add(4,5);
+console.log(sum);
